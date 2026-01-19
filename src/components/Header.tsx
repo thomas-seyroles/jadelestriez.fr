f9aa5f9ff4db4bff2021b227b6f5824bbeff721f
Thomas Seyroles <seytom00@gmail.com>
2026-01-19 11:01:16 +0100

    fix(header): resolve logo sticky hover opacity issue using motion props

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,11 @@ export default function Header() {
       transition={{ duration: 0.5 }}
     >
       <div className="logo">
-        <Link href="/">
+        <Link 
+          href="/"
+          whileHover={{ opacity: 0.6 }}
+          whileTap={{ opacity: 1, scale: 0.95 }}
+        >
           <img src={logo} alt="Logo JADE" />
         </Link>
       </div>
